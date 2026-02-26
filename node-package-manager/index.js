2f074f693d46e8381d0eefc5777728a957ef8277
yoeda11 <yoeda11@gmail.com>
2026-02-26 14:35:55 +0700

    npm

---
+++ node-package-manager/index.js
@@ -0,0 +1,5 @@
+import moment from "moment";
+
+const date = moment().format("MMM Do YY");
+console.log(date);
+
---
+++ node-package-manager2/index.js
@@ -0,0 +1,7 @@
+// TODO 1
+import lodash from "lodash";
+
+const myOddEventArray = lodash.partition([1,2,3,4,5,6], (n) => n % 2);
+console.log(myOddEventArray);
+
+
