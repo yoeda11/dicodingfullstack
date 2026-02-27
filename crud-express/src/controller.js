a59edc7329d0c0029b2f03e822dda60f0fbc467c
yoeda11 <yoeda11@gmail.com>
2026-02-27 13:49:29 +0700

    view

--- crud-express/src/controller.js
+++ crud-express/src/controller.js
@@ -2,12 +2,12 @@ import {nanoid} from "nanoid";
 import notes from "./notes.js";
 
 export const createNote = (req, res, next) => {
-    const {title = 'untitled', tag, body} = req.body;
+    const {title = 'untitled', tags, body} = req.body;
     const id = nanoid(16);
 const createdAt = new Date().toISOString();
-const updateAt = createdAt;
+const updatedAt = createdAt;
 
-    const newNote = {title, tags, body, id, createdAt, updateAt};
+    const newNote = {title, tags, body, id, createdAt, updatedAt};
     notes.push(newNote);
 
     const isSuccess = notes.filter((note) => note.id === id).length > 0;
@@ -23,11 +23,34 @@ const updateAt = createdAt;
 
     return res.status(500).json({
         status: 'fail',
-        message: 'Catatan gagal ditambahkan'
+        message: 'Catatan gagal ditambahkan',
     });
 
 };
 
+
+export const getNotes = (req, res) => {
+    return res.json({
+        status: 'success',
+        data: { notes }
+    });
+};
+
+
+export const getNoteById = (req, res) => {
+    const { id } = req.params;
+  const note = notes.find((n) => n.id === id);
+  if (note) {
+    return res.json({
+      status: 'success',
+      data: { note }
+    });
+  }
+  return res.status(404).json({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan'
+  });
+};
 
 
 
--- crud-express/src/server.js
+++ crud-express/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import routes from './routes.js';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
+
+app.use(cors({
+    origin: 'http://notesapp-v1.dicodingacademy.com'
+}));
 
 app.use(express.json());
 app.use('/', routes);
@@ -12,6 +17,8 @@ app.use('/', routes);
 app.listen(port, () => {
     console.log(`Server running at http://${host}:${port}`);
 });
+
+
 
 
 
